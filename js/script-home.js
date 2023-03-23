604a8ab8a9cf11f04c5452c975e27faa382c3038
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-23 17:50:22 +0100

    fix funzione randomica in index.html

--- js/script-home.js
+++ js/script-home.js
@@ -1,11 +1,10 @@
-const albumHeader = 'https://striveschool-api.herokuapp.com/api/deezer/album/366045987';
-const cardSmall1 = 'https://striveschool-api.herokuapp.com/api/deezer/album/309377597';
-const cardSmall2 = 'https://striveschool-api.herokuapp.com/api/deezer/album/59853992';
+const KEY = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=pink-floyd';
+
+let objectAlbums={};
+
 
 window.onload = async () => {
-    creaCardHeader(albumHeader);
-    creaCardSmall1(cardSmall1);
-    //creaCardSmall2(cardSmall2);
+    creaCardHeader(KEY);
 }
 
 async function creaCardHeader(params) {
@@ -14,35 +13,44 @@ async function creaCardHeader(params) {
         const response = await fetch(params, {
         });
 
-        const album = await response.json();
+        const albums = await response.json();
+
+        console.log(albums);
+
+        objectAlbums={ ...albums };
+
+        let numRandom=Math.floor(Math.random() * 24);
+
+        let album = objectAlbums.data[numRandom];
 
         console.log(album);
-
+        
         assegnaHeader(album);
     } catch (error) {
         console.log(error);
     }
 }
-function assegnaHeader(album) {
+function assegnaHeader(params) {
+
     let cardDiv = document.getElementById("cardHeader");
     cardDiv.innerHTML = "";
     cardDiv.innerHTML = `
     <div class="mb-3 d-flex justify-content-center">
         <img class="justify-content-sm-center rounded-1 imgResponsiveHome"
-        width="200px" src="${album.cover_medium}" alt="${album.title}">
+        width="200px" src="${params.album.cover_medium}" alt="${params.album.title}">
     </div>
     <div class="mt-sm-0 ms-2 ms-md-3 mt-md-5 d-flex flex-column">
         <div>
             <h1 class="fs-6 text-white">ALBUM</h1>
         </div>
         <div  class="mb-3 justify-content-sm-center">
-            <h1  class="fs-1 text-white">${album.title}</h1>
+            <h1  class="fs-1 text-white">${params.album.title}</h1>
         </div>
         <div>
             <a href="#">
-                <img class="rounded-5" width="25px" src="${album.artist.picture_medium}"
-                alt="${album.artist.name}">
-                <h1 class="fs-6 d-inline">${album.artist.name}</h1>
+                <img class="rounded-5" width="25px" src="${params.artist.picture_medium}"
+                alt="${params.artist.name}">
+                <h1 class="fs-6 d-inline">${params.artist.name}</h1>
             </a>
         </div>
         <div class="my-3">
@@ -52,7 +60,7 @@ function assegnaHeader(album) {
     </div>
     `;
 }
-async function creaCardSmall1(params) {
+/*async function creaCardSmall1(params) {
 
     try {
         const response = await fetch(params, {
@@ -79,7 +87,7 @@ function assegnaCardSmall1(album) {
         </div>
     </div>
     `;
-}
+}*/
 /*async function creaCardSmall2(params) {
 
     try {
