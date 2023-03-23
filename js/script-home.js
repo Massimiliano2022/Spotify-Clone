7fbb1244d08af823668b08749e6bb217b45ada6e
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-23 16:23:15 +0100

    fix js home

--- js/script-home.js
+++ js/script-home.js
@@ -1,9 +1,7 @@
 const albumHeader = 'https://striveschool-api.herokuapp.com/api/deezer/album/366045987';
-const cardSmall1  =  'https://striveschool-api.herokuapp.com/api/deezer/album/309377597';
 
 window.onload = async () => {
     creaCardHeader(albumHeader);
-
 }
 
 async function creaCardHeader(params) {
@@ -16,38 +14,38 @@ async function creaCardHeader(params) {
 
         console.log(album);
 
-        assegnaImgHeader(album);
-        assegnaTitleHeader(album);
-        assegnaArtistaHeader(album);
-
-
+        assegnaHeader(album);
     } catch (error) {
         console.log(error);
     }
 }
 
-function assegnaImgHeader(album) {
+function assegnaHeader(album) {
     let cardDiv = document.getElementById("cardHeader");
+    cardDiv.innerHTML="";
     cardDiv.innerHTML = `
-    <img id="imgHeader" class="justify-content-sm-center rounded-1 imgResponsiveHome" width="200px" src="${album.cover_medium}" alt="Cover">
+    <div class="mb-3 d-flex justify-content-center">
+        <img class="justify-content-sm-center rounded-1 imgResponsiveHome"
+        width="200px" src="${album.cover_medium}" alt="${album.title}">
+    </div>
+    <div class="mt-sm-0 ms-2 ms-md-3 mt-md-5 d-flex flex-column">
+        <div>
+            <h1 class="fs-6 text-white">ALBUM</h1>
+        </div>
+        <div  class="mb-3 justify-content-sm-center">
+            <h1  class="fs-1 text-white">${album.title}</h1>
+        </div>
+        <div>
+            <a href="#">
+                <img class="rounded-5" width="25px" src="${album.artist.picture_medium}"
+                alt="${album.artist.name}">
+                <h1 class="fs-6 d-inline">${album.artist.name}</h1>
+            </a>
+        </div>
+        <div class="my-3">
+            <button type="button" class="btn btn-success text-dark">Play</button>
+            <button type="button" class="btn btn-light">Salva</button>
+        </div>
+    </div>
     `;
 }
-
-function assegnaTitleHeader(album) {
-    let cardDiv = document.getElementById("cardTitle");
-    cardDiv.innerHTML = `
-        <h1 id="titleHeader" class="fs-1 text-white">${album.title}</h1>
-        `;
-}
-
-function assegnaArtistaHeader(album) {
-
-    let cardDiv = document.getElementById("cardArtist");
-    cardDiv.innerHTML = `
-        <a href="#">
-            <img class="rounded-5" width="30px" src="${album.artist.picture_small}" alt="logo 3">
-            <h1 class="fs-6 d-inline">${album.artist.name}</h1>
-        </a>
-    `;
-}
-
